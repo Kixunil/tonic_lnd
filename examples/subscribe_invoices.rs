0633bf6f04c15b50efca27533c4e77edf69d865c
Jonathan Zernik <yzernik@gmail.com>
2022-07-21 19:28:39 -0700

    Sync with upstream repo (changes in examples) (#14)

--- examples/subscribe_invoices.rs
+++ examples/subscribe_invoices.rs
@@ -1,6 +1,4 @@
-// This example only fetches and prints the node info to the standard output similarly to
-// `lncli getinfo`.
-//
+// This program connects to LND and prints out all incoming invoices as they settle.
 // This program accepts four arguments: host, port, cert file, macaroon file
 
 #[tokio::main]
@@ -28,18 +26,32 @@ async fn main() {
         .into_string()
         .expect("macaroon_file is not UTF-8");
 
-    // Connecting to LND requires only host, port, cert file, macaroon file
+    // Connecting to LND requires only address, cert file, and macaroon file
     let mut client = tonic_openssl_lnd::connect_lightning(host, port, cert_file, macaroon_file)
         .await
         .expect("failed to connect");
 
-    let info = client
-        // All calls require at least empty parameter
-        .get_info(tonic_openssl_lnd::lnrpc::GetInfoRequest {})
+    let mut invoice_stream = client
+        .subscribe_invoices(tonic_openssl_lnd::lnrpc::InvoiceSubscription {
+            add_index: 0,
+            settle_index: 0,
+        })
         .await
-        .expect("failed to get info");
+        .expect("Failed to call subscribe_invoices")
+        .into_inner();
 
-    // We only print it here, note that in real-life code you may want to call `.into_inner()` on
-    // the response to get the message.
-    println!("{:#?}", info);
+    while let Some(invoice) = invoice_stream
+        .message()
+        .await
+        .expect("Failed to receive invoices")
+    {
+        if let Some(state) =
+            tonic_openssl_lnd::lnrpc::invoice::InvoiceState::from_i32(invoice.state)
+        {
+            // If this invoice was Settled we can do something with it
+            if state == tonic_openssl_lnd::lnrpc::invoice::InvoiceState::Settled {
+                println!("{:?}", invoice);
+            }
+        }
+    }
 }
